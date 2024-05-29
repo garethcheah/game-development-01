8878d3c4e5a68bd63065a7121c645d9366680fdc
garethcheah <garethcheah@gmail.com>
2024-05-28 20:42:11 -0700

    Add code to apply force to ball. Add physic materials to bowling ball and alley.

--- Assets/Scripts/BowlingBall.cs
+++ Assets/Scripts/BowlingBall.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BowlingBall : MonoBehaviour
+namespace BowlingAssingment
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BowlingBall : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private float appliedForce = 10f;
+        private Rigidbody rigidbody;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Awake is called before Start
+        private void Awake()
+        {
+            
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                AddForce();
+            }
+        }
+
+        // FixedUpdate is called every fixed framerate frame
+        private void FixedUpdate()
+        {
+            
+        }
+
+        private void AddForce()
+        {
+            rigidbody.AddForce(Vector3.right.normalized * appliedForce, ForceMode.Impulse);
+        }
     }
 }
